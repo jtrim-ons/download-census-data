5411ce5c3a9da88a7f496b792cadf96fb40d5a0f
James Trimble <jtrim.ons@gmail.com>
2021-11-22 21:29:01 +0000

    Get data in chunks, and get some 1991 Census data

--- get_stuff.py
+++ get_stuff.py
@@ -12,6 +12,7 @@ from census.cache import get_page
 api_codelist_url = "https://www.nomisweb.co.uk/api/v01/dataset/{DATASET}/{CELL}.def.sdmx.json"
 api_data_url = "https://www.nomisweb.co.uk/api/v01/dataset/{DATASET}.data.csv?date=latest&geography={GEO_TYPES}&{CELL}={CELLS}&measures={MEASURES}&select=date_name,geography_name,geography_code,geography_type,geography_typecode,{CELL},{CELL}_name,{CELL}_type,measures,measures_name,obs_value,obs_status_name?uid={UID}"
 geo_types_2011 = "TYPE464,TYPE480,TYPE499"
+record_limit = 100000
 
 p = get_page('https://www.nomisweb.co.uk/api/v01/dataset/def.sdmx.json')
 
@@ -32,14 +33,6 @@ def is_correct_year(keyfamily, year):
 def filtered_by_year(keyfamilies, year):
     return [f for f in keyfamilies if is_correct_year(f, year)]
 
-datasets_uv_ks_2001 = [
-    ds for ds in filtered_by_year(keyfamilies, 2001)
-    if (
-        ds["name"]["value"][:2] == "UV" or
-        ds["name"]["value"][:2] == "KS"
-    )
-]
-    
 def check_expected_dimensions(dims):
     if len(dims) != 4:
         return False
@@ -59,33 +52,9 @@ def get_codes_from_nomis_api(table_code, codelist_name):
         code["value"] for code in
         json.loads(get_page(url))["structure"]["codelists"]["codelist"][0]["code"]
     ]
-    return ",".join(str(c) for c in codes)
+    return codes
 
-con = sqlite3.connect('big-census.db')
-cur = con.cursor()
-
-for ds in datasets_uv_ks_2001:
-    year = 2001
-    table_code = ds["id"]
-    print(table_code)
-    table_name = ds["name"]["value"]
-    dims = [d["conceptref"] for d in ds["components"]["dimension"]]
-    if not check_expected_dimensions(dims):
-        continue
-    cell_dim = get_cell_dim(dims)
-
-    measure_codes = get_codes_from_nomis_api(table_code, 'MEASURES')
-    cell_codes = get_codes_from_nomis_api(table_code, cell_dim)
-
-    url = api_data_url.format(
-        DATASET=table_code,
-        GEO_TYPES=geo_types_2011,
-        CELL=cell_dim,
-        CELLS=cell_codes,
-        MEASURES=measure_codes,
-        UID=os.environ['NOMIS_UID']
-    )
-    dataset_csv = get_page(url)
+def get_csvs_and_add_to_db(year, table_code, table_name, cell_dim, cell_codes, measure_codes, cur, con):
     sql = '''INSERT OR IGNORE INTO census_values (
         year,
         table_id,
@@ -103,16 +72,74 @@ for ds in datasets_uv_ks_2001:
         obs_value,
         obs_status_name
     ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''
-    lines = dataset_csv.splitlines()
-    db_lines = []
-    for i, line in enumerate(csv.reader(lines)):
+    record_offset = 0
+    while True:
+        url = api_data_url.format(
+            DATASET=table_code,
+            GEO_TYPES=geo_types_2011,
+            CELL=cell_dim,
+            CELLS=cell_codes,
+            MEASURES=measure_codes,
+            UID=os.environ['NOMIS_UID']
+        ) + '&RecordLimit={}&RecordOffset={}'.format(record_limit, record_offset)
+        dataset_csv = get_page(url)
+        lines = dataset_csv.splitlines()
+        db_lines = []
+        for i, line in enumerate(csv.reader(lines)):
+            if i == 0:
+                continue
+            db_line = [year, table_code, table_name] + line
+            db_line = [None if item == "" else item for item in db_line]
+            db_lines.append(db_line)
+        cur.executemany(sql, db_lines)
         if i == 0:
-            continue
-        db_line = [2001, table_code, table_name] + line
-        db_line = [None if item == "" else item for item in db_line]
-        db_lines.append(db_line)
-
-    cur.executemany(sql, db_lines)
+            break   # No records returned in this CSV, so we're done.
+        record_offset += record_limit
     con.commit()
 
-con.close()
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    # https://stackoverflow.com/a/312464/3347737
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+def add_dataset_to_db(ds, year, cur, con):
+    table_code = ds["id"]
+    print(table_code)
+    table_name = ds["name"]["value"]
+    dims = [d["conceptref"] for d in ds["components"]["dimension"]]
+    if not check_expected_dimensions(dims):
+        print("Skipping {}; unexpected dimensions.".format(table_name))
+        return
+    cell_dim = get_cell_dim(dims)
+
+    measure_codes = ",".join(str(c) for c in get_codes_from_nomis_api(table_code, 'MEASURES'))
+    cell_codes = get_codes_from_nomis_api(table_code, cell_dim)
+
+    for cell_codes_chunk in chunks(cell_codes, 10):
+        get_csvs_and_add_to_db(year, table_code, table_name, cell_dim, ",".join(str(c) for c in cell_codes_chunk), measure_codes, cur, con)
+
+def main():
+    con = sqlite3.connect('big-census.db')
+    cur = con.cursor()
+
+    datasets_uv_ks_2001 = [
+        ds for ds in filtered_by_year(keyfamilies, 2001)
+        if (
+            ds["name"]["value"][:2] == "UV" or
+            ds["name"]["value"][:2] == "KS"
+        )
+    ]
+    for ds in datasets_uv_ks_2001:
+        add_dataset_to_db(ds, 2001, cur, con)
+
+    dataset_lbs_1991 = [
+        ds for ds in filtered_by_year(keyfamilies, 1991)
+        if ds["name"]["value"] == "1991 census - local base statistics"
+    ][0]
+    add_dataset_to_db(dataset_lbs_1991, 1991, cur, con)
+
+    con.close()
+
+if __name__ == "__main__":
+    main()
